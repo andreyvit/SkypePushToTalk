c2a029cbf1c14ff793058e55fa6b61d6650a34d6
Andrey Tarantsov <andreyvit@gmail.com>
2010-10-05 13:20:27 +0700

    Create an empty Cocoa project in Xcode 3.2.2

---
+++ SkypePushToTalk/SkypePushToTalkAppDelegate.m
@@ -0,0 +1,12 @@
+
+#import "SkypePushToTalkAppDelegate.h"
+
+@implementation SkypePushToTalkAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
---
+++ SkypePushToTalk/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SkypePushToTalk
+//
+//  Created by Andrey Tarantsov on 10/5/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
