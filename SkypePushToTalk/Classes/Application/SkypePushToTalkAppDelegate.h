9a26a784970c63bfb71d7338daa9a04613021840
Andrey Tarantsov <andreyvit@gmail.com>
2010-10-05 14:24:42 +0700

    Open menu when status bar icon is clicked

--- SkypePushToTalk/Classes/Application/SkypePushToTalkAppDelegate.h
+++ SkypePushToTalk/Classes/Application/SkypePushToTalkAppDelegate.h
@@ -4,11 +4,15 @@
 @class DDHotKeyCenter;
 
 @interface SkypePushToTalkAppDelegate : NSObject <NSApplicationDelegate> {
+	IBOutlet NSMenu *_statusItemMenu;
+
     NSWindow *window;
 	NSStatusItem *_statusItem;
 	DDHotKeyCenter *_hotKeyCenter;
 }
 
 @property (assign) IBOutlet NSWindow *window;
+
+- (IBAction)quitApplication:(id)sender;
 
 @end
--- SkypePushToTalk/Classes/Application/SkypePushToTalkAppDelegate.m
+++ SkypePushToTalk/Classes/Application/SkypePushToTalkAppDelegate.m
@@ -14,16 +14,15 @@ enum {
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
 	_statusItem = [[[NSStatusBar systemStatusBar] statusItemWithLength:NSSquareStatusItemLength] retain];
 	[_statusItem setTitle:@"PTT"];
-	[_statusItem setTarget:self];
 	[_statusItem setHighlightMode:YES];
-	[_statusItem setAction:@selector(quitApp)];
+	[_statusItem setMenu:_statusItemMenu];
 	
 	_hotKeyCenter = [[DDHotKeyCenter alloc] init];
 	BOOL hotKeyOK = [_hotKeyCenter registerHotKeyWithKeyCode:kVK_F1 modifierFlags:0 target:self action:@selector(pushToTalkPressed:) object:nil];
 	NSAssert(hotKeyOK, @"Cannot register hotkey");
 }
 
-- (IBAction)quitApp {
+- (IBAction)quitApplication:(id)sender {
 	[[NSApplication sharedApplication] terminate:self];
 }
 
