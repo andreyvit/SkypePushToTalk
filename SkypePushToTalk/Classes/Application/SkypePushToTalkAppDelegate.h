9609aeee38a93bb66af1d8cc1f70ec0e6f4a8f57
Andrey Tarantsov <andreyvit@gmail.com>
2010-10-05 14:14:06 +0700

    Register a global hot key

--- SkypePushToTalk/Classes/Application/SkypePushToTalkAppDelegate.h
+++ SkypePushToTalk/Classes/Application/SkypePushToTalkAppDelegate.h
@@ -1,9 +1,12 @@
 
 #import <Cocoa/Cocoa.h>
 
+@class DDHotKeyCenter;
+
 @interface SkypePushToTalkAppDelegate : NSObject <NSApplicationDelegate> {
     NSWindow *window;
 	NSStatusItem *_statusItem;
+	DDHotKeyCenter *_hotKeyCenter;
 }
 
 @property (assign) IBOutlet NSWindow *window;
--- SkypePushToTalk/Classes/Application/SkypePushToTalkAppDelegate.m
+++ SkypePushToTalk/Classes/Application/SkypePushToTalkAppDelegate.m
@@ -1,5 +1,11 @@
 
 #import "SkypePushToTalkAppDelegate.h"
+#import "DDHotKeyCenter.h"
+
+// copied from Carbon to avoid inclusion
+enum {
+	kVK_F1                        = 0x7A,
+};
 
 @implementation SkypePushToTalkAppDelegate
 
@@ -11,9 +17,17 @@
 	[_statusItem setTarget:self];
 	[_statusItem setHighlightMode:YES];
 	[_statusItem setAction:@selector(quitApp)];
+	
+	_hotKeyCenter = [[DDHotKeyCenter alloc] init];
+	BOOL hotKeyOK = [_hotKeyCenter registerHotKeyWithKeyCode:kVK_F1 modifierFlags:0 target:self action:@selector(pushToTalkPressed:) object:nil];
+	NSAssert(hotKeyOK, @"Cannot register hotkey");
 }
 
 - (IBAction)quitApp {
+	[[NSApplication sharedApplication] terminate:self];
+}
+
+- (void)pushToTalkPressed:(NSEvent *)event {
 	[[NSApplication sharedApplication] terminate:self];
 }
 
